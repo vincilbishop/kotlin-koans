6fc1d1262098a5ceb4f6f8e2b3153da8702f6195
vsurge <vbishop@surgeforward.com>
2019-02-22 10:09:24 -0600

    Updated for default parameters (#2)
    * Unit tests
    
    * Updated for default values test

--- src/i_introduction/_3_Default_Arguments/n03DefaultArguments.kt
+++ src/i_introduction/_3_Default_Arguments/n03DefaultArguments.kt
@@ -14,15 +14,15 @@ fun todoTask3(): Nothing = TODO(
     documentation = doc2(),
     references = { name: String -> JavaCode3().foo(name); foo(name) })
 
-fun foo(name: String, number: Number = 0, toUpperCase: Boolean = false): String = {
+fun foo(name: String, number: Number = 0, toUpperCase: Boolean = false): String {
 
     return (if (toUpperCase) name.toUpperCase() else name) + number
 }
 
 fun task3(): String {
     // todoTask3()
-    return (foo("a") +
+    return (foo("a", number = 42) +
             foo("b", number = 1) +
-            foo("c", toUpperCase = true) +
+            foo("c", toUpperCase = true, number = 42) +
             foo(name = "d", number = 2, toUpperCase = true))
 }
