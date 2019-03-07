f2fe868ecb1ecebd90190c130466a7b3e7d1b5a2
vSurge <vbishop@surgeforward.com>
2019-03-07 10:13:37 -0500

    Fixed Lambda logic

--- src/i_introduction/_4_Lambdas/n04Lambdas.kt
+++ src/i_introduction/_4_Lambdas/n04Lambdas.kt
@@ -27,5 +27,5 @@ fun task4(collection: Collection<Int>): Boolean {
     val sum = { x: Int, y: Int -> x + y }
     val square: (Int) -> Int = { x -> x * x }
 
-    return sum(1, square(2)) == 5
+    return sum(collection.toIntArray()[1], square(collection.toIntArray()[0])) == collection.toIntArray()[2]
 }
